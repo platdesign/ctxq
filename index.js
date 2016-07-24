daabba2e477eeadd2737ff644321feba5c23db24
Christian Blaschke <mail@platdesign.de>
2016-07-24 23:37:04 +0200

    ...

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 'use strict';
 
 
-module.exports = function() {
+module.exports = function ctxq() {
 
-	return function ctxq() {
+	let service = {};
+	let queue = [];
 
-		let service = {};
-		let queue = [];
-
-		service.push = function(key, fn) {
-			queue.push((scope) => {
-				if(key && fn) {
-					return fn(scope).then((res) => scope[key] = res);
-				}
-				return key(scope);
-			});
-		};
-
-		service.run = function(scope) {
-			scope = scope || {};
-
-			return queue.reduce((p, fn) => p.then(() => fn(scope)), Promise.resolve()).then(() => scope);
-		};
-
-		return service;
+	service.push = function(key, fn) {
+		queue.push((scope) => {
+			if (key && fn) {
+				return fn(scope).then((res) => scope[key] = res);
+			}
+			return key(scope);
+		});
 	};
+
+	service.run = function(scope) {
+		scope = scope || {};
+
+		return queue.reduce((p, fn) => p.then(() => fn(scope)), Promise.resolve()).then(() => scope);
+	};
+
+	return service;
 
 };
